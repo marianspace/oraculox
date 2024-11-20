ee41b86644beea878fcefe05da598df498f59c08
Mariana Constanza Abalo <abalomariana@gmail.com>
2024-11-19 22:36:07 -0300

    change3

--- script.js
+++ script.js
@@ -10,19 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
       "mmmmmm",
       "Recorcholis",
       "No lo creo.",
-      "El día menos pensado.",
+      "El día menos pensado, será",
       "Decretálo"
     ];
 
     const boxrespuesta = document.getElementById('response');
-
-
     boxrespuesta.textContent = "El Oráculo esta pensando..."
+    boxrespuesta.classList.add('fade-out');
 
     setTimeout(() => {
       const randomIndex = Math.floor(Math.random() * responses.length);
       const randomResponse = responses[randomIndex];
       boxrespuesta.textContent = randomResponse;
-    }, 5000);
-  });
-  
+      boxrespuesta.classList.remove('fade-out');
+      boxrespuesta.classList.add('fade-in');
+      boxrespuesta.classList.add("respuesta");
+    }, 5000);    
+
+    let redirected = false;
+
+    document.addEventListener("mousemove", () => {
+    if (!redirected) {
+    redirected = true;
+    window.location.href = "index.html";
+    }})
+})
